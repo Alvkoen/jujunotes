38411ab2969d5ab7459615218de17b8fbce2fa05
Aleksandra Koen <aleksandra.koen@wolt.com>
2022-12-06 18:16:53 +0200

    Initial Commit

---
+++ jujunotes/jujunotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  jujunotesApp.swift
+//  jujunotes
+//
+//  Created by Aleksandra Koen on 6.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct jujunotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
